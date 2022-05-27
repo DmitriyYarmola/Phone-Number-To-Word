e4e50c588dcefc563fde5fe13f20f03e8c416f9b
Dmitriy Yarmola <dmitriy.yarmola@gmail.com>
2022-05-27 21:15:24 +0300

    Added input handler

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
+import { useEffect, useState } from 'react'
+import { useDebounce } from './hooks'
+import { Input } from './components'
+
 export const App = () => {
+	const [value, setValue] = useState('')
+	const [word, setWord] = useState('')
+	const debouncedValue = useDebounce(value, 500)
+
+	const onChange = (event) => {
+		const value = event.currentTarget.value
+		setValue(value)
+	}
+
 	return (
 		<div>
-			Initial
+			<label>
+				Enter your number: <Input value={value} onChange={onChange} />
+			</label>
+			<p>Converted word: {word}</p>
 		</div>
 	)
 }
