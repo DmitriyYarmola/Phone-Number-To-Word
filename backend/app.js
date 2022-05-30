5e9510dfb23b3fc49e2336a49b9badb746ac021a
Dmitriy Yarmola <dmitriy.yarmola@gmail.com>
2022-05-30 20:56:21 +0300

    Added number convertor

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
-const http = require('http')
+const http = require('http');
 const { getReqData } = require("./utils/getRaquestData");
+const { Controller } = require('./controller')
+
+
 const PORT = process.env.PORT || 5000;
 
+const converter = new Controller()
 
 const server = http.createServer(async (req, res) => {
 
@@ -9,14 +13,17 @@ const server = http.createServer(async (req, res) => {
 
     if (url === "/api/convert" && method === "POST") {
 
-        res.writeHead(200, { "Content-Type": "application/json" });
+        res.writeHead(200, {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"});
 
         const data = await getReqData(req)
 
-        res.end();
-    }
+        if (data) {
+            const {number} = JSON.parse(data)
+            const result = await converter.convertNumber(number)
+            res.end(JSON.stringify({result}))
+        }
 
-    else {
+    } else {
         res.writeHead(404, { "Content-Type": "application/json" });
         res.end(JSON.stringify({ message: "Route not found" }));
     }
---
+++ backend/controller.js
@@ -0,0 +1,35 @@
+const { phoneLetters } = require("./data");
+
+class Controller {
+    constructor() {
+        this.phoneLetters = phoneLetters;
+    }
+
+    convertNumber(number) {
+        return new Promise((resolve) => {
+            const numbers = number.split('')
+            const numberLetters = numbers.map((number) => this.phoneLetters[number]);
+
+            const concat = (letters, current, ...args) => {
+                if (numberLetters[current]) {
+                    const part = letters.map((letter, idx) => {
+                        return (
+                            (idx === 0 ? letter : args.join("") + letter) +
+                            concat(numberLetters[current + 1], current + 1, ...args, letter)
+                        );
+                    });
+
+                    return part.join(",");
+                }
+
+                return "";
+            };
+
+            const result = concat(numberLetters[0], 0, "");
+
+            resolve(result.split(','));
+        })
+    }
+}
+
+module.exports = { Controller };
